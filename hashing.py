68b4ad43dd4de7b8f986f3e2430e3b7458b3abc1
oof <muazalijutt@gmail.com>
2021-08-08 11:11:52 +0500

    hashfunction fixed

--- hashing.py
+++ hashing.py
@@ -2,13 +2,13 @@ from hashlib import sha256
 
 def SignString(string,private_key):
 	string_encoded=string.encode()
-	hash = int.from_bytes(hashlib.sha256(string_encoded).digest(), byteorder='big')
+	hash = int.from_bytes(sha256(string_encoded).digest(), byteorder='big')
 	signature = pow(hash, private_key['d'], private_key['n'])
 	return signature
 
 def VerifySignature(string,signature,public_key):
 	string_encoded=string.encode()
-	hash = int.from_bytes(hashlib.sha256(string_encoded).digest(), byteorder='big')
+	hash = int.from_bytes(sha256(string_encoded).digest(), byteorder='big')
 	hashSignature = pow(signature, public_key['e'], public_key['n'])
 	return hash == hashSignature
 
